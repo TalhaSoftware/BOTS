fc18964a19e4a241adc16a08722e529f344baeff
Talha Yazılım <62337075+TalhaSoftware@users.noreply.github.com>
2021-02-18 19:42:32 +0300

    Update DovizBotuTelegram.py

--- DovizBotuTelegram.py
+++ DovizBotuTelegram.py
@@ -21,8 +21,8 @@ browser = webdriver.Firefox(options=options)
 
 def telegram_bot_sendtext(bot_message):
     
-    bot_token = '1147510032:AAFJm0NvPRjUEwf4szrDwlyPT_9u2yi2I-I'
-    bot_chatID = '989789437'
+    bot_token = ''
+    bot_chatID = ''
     send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
 
     response = requests.get(send_text)
@@ -91,7 +91,7 @@ def echo(update: Update, context: CallbackContext) -> None:
 def main():
     """Start the bot."""
     
-    updater = Updater("1147510032:AAFJm0NvPRjUEwf4szrDwlyPT_9u2yi2I-I", use_context=True)
+    updater = Updater("", use_context=True)
 
     dispatcher = updater.dispatcher
 
